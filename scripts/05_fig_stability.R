f0406627906dacae0b087550e0369a15e464d371
Quang Nguyen <nminhquang.99@gmail.com>
2023-03-11 09:28:04 -0500

    more work on methods

--- scripts/05_fig_stability.R
+++ scripts/05_fig_stability.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(here)
+
 games <- read_csv(here("data", "games.csv"))
 source(here("scripts", "04_fig_cor_pressure.R"))
 
@@ -8,7 +11,7 @@ min_100_snaps <- test |>
   filter(n_plays >= 100 & !is.infinite(strain)) |> 
   pull(displayName)
 
-first <- test |>
+strain_first <- test |>
   left_join(select(games, gameId, week)) |> 
   filter(displayName %in% min_100_snaps, week %in% 1:4, !is.infinite(strain)) |> 
   group_by(nflId) |>
@@ -22,7 +25,7 @@ first <- test |>
   filter(pos %in% c("OLB", "DE", "DT", "NT")) |> 
   select(name, pos, first_strain_rate = strain_rate)
 
-last <- test |>
+strain_last <- test |>
   left_join(select(games, gameId, week)) |> 
   filter(displayName %in% min_100_snaps, week %in% 5:8, !is.infinite(strain)) |> 
   group_by(nflId) |>
@@ -37,8 +40,8 @@ last <- test |>
   arrange(-strain_rate) |> 
   select(name, pos, last_strain_rate = strain_rate)
 
-fig_stability <- first |> 
-  full_join(last) |>
+fig_stability <- strain_first |> 
+  full_join(strain_last) |>
   mutate(pos = factor(pos, levels = c("OLB", "DE", "DT", "NT"))) |> 
   ggplot(aes(first_strain_rate, last_strain_rate)) +
   geom_smooth(method = "lm", se = FALSE, linewidth = 1.5, color = "black", alpha = 0.5) +
@@ -53,3 +56,4 @@ fig_stability <- first |>
           legend.text = element_text(size = rel(0.8)),
           axis.text = element_text(size = rel(0.8)),
           panel.grid.minor = element_blank())
+
