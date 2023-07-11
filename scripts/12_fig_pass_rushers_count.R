e7032befabb4d5ab0b1121cc08d2c0bebd58713d
Quang Nguyen <nminhquang.99@gmail.com>
2023-07-10 21:50:43 -0400

    number of blockers code

--- scripts/12_fig_pass_rushers_count.R
+++ scripts/12_fig_pass_rushers_count.R
@@ -17,3 +17,40 @@ pass_rushers_count <- pff_full |>
   scale_x_continuous(breaks = 1:8) +
   labs(x = "Number of pass rushers",
        y = "Count")
+
+pass_blockers_count <- pff_full |> 
+  filter(pff_role == "Pass Block") |> 
+  count(gameId, playId) |> 
+  count(n) |> 
+  ggplot(aes(n, nn)) +
+  geom_col() +
+  scale_x_continuous(breaks = 1:8) +
+  labs(x = "Number of pass blockers",
+       y = "Count")
+
+
+rushers_blockers <- pff_full |> 
+  filter(pff_role == "Pass Rush") |> 
+  count(gameId, playId, name = "n_rushers") |> 
+  full_join(
+    pff_full |> 
+      filter(pff_role == "Pass Block") |> 
+      count(gameId, playId, name = "n_blockers")
+  )
+
+rushers_blockers |> 
+  select(contains("n_")) |> 
+  drop_na() |> 
+  cor()
+
+rushers_blockers |> 
+  ggplot(aes(n_rushers, n_blockers)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+rushers_blockers |> 
+  count(n_rushers, n_blockers, sort = TRUE) |> 
+  ggplot(aes(n_rushers, n_blockers)) +
+  geom_tile(aes(fill = n), color = "white") +
+  coord_fixed()
